7b2bde1f316cbe433d52b5ff23f02cbb03adfe7f
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2023-01-30 08:44:34 -0300

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,41 +2,8 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.library").version("7.3.1").apply(false)
     kotlin("multiplatform").version("1.7.10").apply(false)
-    id("maven-publish")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenKotlin") {
-            pom {
-                name.set("Validation VTVinicius")
-                description.set("A concise description of my library")
-                url.set("https://github.com/VTVinicius/Validations")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("vini")
-                        name.set("Vinicius Teixeira")
-                        email.set("vinicius16.ifc@gmail.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:https://github.com/VTVinicius/Validations")
-                    developerConnection.set("scm:https://github.com/VTVinicius/Validations")
-                    url.set("https://github.com/VTVinicius/Validations")
-                }
-            }
-        }
-    }
-}
-
-group = "br.com.vtvinicius.validations"
-version = "1.0.0"
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
