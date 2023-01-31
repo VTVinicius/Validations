a59c53ef97413fac028bb4891bc0cbca024751cd
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2023-01-31 08:23:08 -0300

    commit

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -66,9 +66,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.VTVinicius"
             artifactId = "validations"
-            version = "1.0.5"
-
-            from(components["kotlin"])
+            version = "1.0.6"
         }
     }
 }
